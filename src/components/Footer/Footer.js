74201a19a10f9ad23888f62dc622b785fa484b5f
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-12-12 07:58:00 -0800

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made by{" "}
+      <a
+        href="https://www.linkedin.com/in/sarojvrc/"
+        style={{ cursor: "pointer" }}
+      >
+        Saroj
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
